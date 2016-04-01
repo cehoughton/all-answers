6ddc3e104558a6d71d4e72aab030557a7b68669c
chris <cehoughton71@gmail.com>
2016-04-01 11:22:41 -0700

    create new question form component

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return this.store.findAll('answer');
+  },
+
+  actions: {
+    save3(params) {
+      var newAnswer = this.store.createRecord('answer', params);
+      newAnswer.save();
+      this.transitionTo('index');
+    },
+
+  saveQuestion3(params) {
+    var newQuestion = this.store.createRecord('question', params);
+    newQuestion.save();
+    this.transitionTo('index');
+   }
+  }
 });
--- app/routes/question.js
+++ app/routes/question.js
@@ -4,4 +4,25 @@ export default Ember.Route.extend({
   maodel(params) {
     return this.store.findRecord('question', params.question_id);
   }
+  actions: {
+    update(question, params) {
+      Object.keys(params).forEach(function(key) {
+        if(params[key]!==undefined) {
+          question.set(key,params[key]);
+        }
+      });
+      question.save();
+      this.transitionTo('index');
+    },
+
+    destroyQuestion(question) {
+    var answer_deletions = question.get('answers').map(function(answer) {
+      return answer.destroyRecord();
+    });
+    Ember.RSVP.all(answer_deletions).then(function() {
+      return question.destroyRecord();
+    });
+    this.transitionTo('index');
+   }
+  }
 });
