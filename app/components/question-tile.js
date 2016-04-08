9e91fd42a8d9a785afb4014199e20d68067fc895
chris <cehoughton71@gmail.com>
2016-04-08 16:27:03 -0700

    service to add favorite questions

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,8 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  bestQuestion: Ember.inject.service(),
   updateQuestionForm: false,
+
   actions: {
+    addToFav(item) {
+      this.get('bestQuestion').add(item);
+    },
     update(question, params) {
       this.sendAction('update', question, params);
     }
